6447e844b94e2e8220f11cbc77478dbd8cd38f68
tyn_Gwen <Gwenyining@gmail.com>
2025-07-20 08:43:36 +0000

    Jul 20, 2025, 8:43 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -4,7 +4,7 @@ Task 1: Add the setup() function
 
 function setup(){
     createCanvas(600,400);
-    background(90,65,);//0-255 background (r,g,b); r-red g-green b-blue
+    background(228,122,);//0-255 background (r,g,b); r-red g-green b-blue
 }
 function draw() {
   // Challenge 1: An eye using ellipse() function
