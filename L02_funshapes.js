48239a5e5f39c2cfbdb633d0ab4edf0a47c851d5
tyn_Gwen <Gwenyining@gmail.com>
2025-07-20 09:02:57 +0000

    Jul 20, 2025, 9:02 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -8,7 +8,7 @@ function setup(){
 }
 function draw() {
     ellipse(100,100,50,50);
-    ellipse(200,100,80,50);
+    ellipse(200,100,50,50);
     
   // Challenge 1: An eye using ellipse() function
 
