49a31cdc953d72b8acf1f6bbc3e0ea0b151a437d
tyn_Gwen <Gwenyining@gmail.com>
2025-07-27 08:21:27 +0000

    Jul 27, 2025, 8:21 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -16,7 +16,7 @@ function draw() {
     triangle(30,75,58,20,86,75);
     fill(25,200,100)
     triangle(35,55,90,55,72,47);
-    quad(20,50,70,)
+    quad(20,50,70,50,70)
 
   // Challenge 1: An eye using ellipse() function
 
