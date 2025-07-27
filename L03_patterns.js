debb81f215ecfe06ecc47014f0b3680199667a00
tyn_Gwen <Gwenyining@gmail.com>
2025-07-27 09:06:59 +0000

    Jul 27, 2025, 9:06 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -10,7 +10,7 @@ function draw() {
     // circle(375,200,100);
     // circle(525,200,100);
     for(let i=0; i<4; i++) {
-      fill(i*50,i*50,i)
+      fill(i*50,i*50,i*50);
       circle(50+i*50,200,40);
     }
   // Recap 1: Repeating Circles
