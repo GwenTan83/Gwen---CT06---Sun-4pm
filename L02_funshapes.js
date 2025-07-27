2cfa12bc396a45a88c18d1668c9c79d470d0e8f8
tyn_Gwen <Gwenyining@gmail.com>
2025-07-27 08:37:28 +0000

    Jul 27, 2025, 8:37 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -19,6 +19,7 @@ function draw() {
     // quad(20,50,70,50,70,100,20,100);
     // quad(20,50,100,50,100,100,20,100);
     // fill(0,0,225) //fill(r,g,b)(0-255);
+    noFill();
     stroke(0,255,0); // colour for outline
     strokeWeight(10); // thickness
     quad(50,162,86,150,50,138,14,150);
