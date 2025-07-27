3a78082440acd526e25974ece82f613caef5fa2e
tyn_Gwen <Gwenyining@gmail.com>
2025-07-27 08:56:25 +0000

    Jul 27, 2025, 8:56 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -5,11 +5,11 @@ function setup() {
   }
   
 function draw() {
-    circle(75,200,100);
-    circle(225,200,100);
-    circle(375,200,100);
-    circle(525,200,100);
-
+    // circle(75,200,100);
+    // circle(225,200,100);
+    // circle(375,200,100);
+    // circle(525,200,100);
+    
   // Recap 1: Repeating Circles
 
   // Task 1: Colour Gradient
