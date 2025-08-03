f9dc57d3e4fa57ab6cd57f218dddf870de5c5217
tyn_Gwen <Gwenyining@gmail.com>
2025-08-03 08:59:19 +0000

    Aug 3, 2025, 8:59 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -28,7 +28,7 @@ function draw() {
     let numCircles=5;
     let totalWidth=diameter*numCircles;
     // let startX=(width-totalWidth)/2 + diameter/2;
-    let startX=(width-totalWidth)
+    let startX=(width-totalWidth)+diameter
 
     for(let i=0; i<5; i++) {
       circle(startX+i*diameter,height/2,diameter);
