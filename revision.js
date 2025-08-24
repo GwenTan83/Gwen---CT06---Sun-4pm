c9c2b9fece78512f7ae8811b9400dd83b3a2937f
tyn_Gwen <Gwenyining@gmail.com>
2025-08-24 08:46:56 +0000

    Aug 24, 2025, 8:46 AM

--- revision.js
+++ revision.js
@@ -7,8 +7,6 @@ function setup(){
 // Exercise 2: Draw 5 circles horizontally across the canvas and fill in with green colour gradient
 function draw(){
     circle(75,200,100)
-    circle()
-    circle()
-    circle()
-    circle()
-}
+    circle(225,200,100);
+    circle(375,200,100);
+    circle(525,200,100);
