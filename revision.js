0d6fff79c7974f4c6adaad9c9ef96a890d2591b9
tyn_Gwen <Gwenyining@gmail.com>
2025-08-24 09:06:56 +0000

    Aug 24, 2025, 9:06 AM

--- revision.js
+++ revision.js
@@ -1,13 +1,15 @@
 // Exercise 1: Draw a square in the middle of the canvas
-function setup(){
-    createCanvas(600, 600);
-    background(220); 
-}
+// function setup(){
+//     createCanvas(600, 600);
+//     background(220); 
+// }
 
-let size = 100;
-function draw(){
-    rect(width/2-size/2, height/2-size/2, size, size)
-}
+// let size = 100;
+// function draw(){
+//     rect(width/2-size/2, height/2-size/2, size, size)
+// }
 
 
-// Exercise 2: Draw 5 circles horizontally across the canvas and fill in with 
+// Exercise 2: Draw 5 circles horizontally across the canvas and fill in with green colour gradient
+
+function setup() {}
