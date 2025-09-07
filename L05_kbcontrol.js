d062d807eeae9356e1a16f5b5f9fe077b1c441a7
tyn_Gwen <Gwenyining@gmail.com>
2025-09-07 08:54:41 +0000

    Sep 7, 2025, 8:54 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -21,50 +21,55 @@
 // }
 //--------------------------------------------------------------recap
 
-let rectSize=50;
+// let rectSize=50;
 
-function setup(){
-    createCanvas(400,400);
-}
+// function setup(){
+//     createCanvas(400,400);
+// }
 
-function draw(){
-    background(220);
+// function draw(){
+//     background(220);
 
-    let centerX = width/2;
-    let centerY = height/2;
+//     let centerX = width/2;
+//     let centerY = height/2;
     
-    let topLeftX = centerX - rectSize/2
-    let topleftY = centerY - rectSize/2
+//     let topLeftX = centerX - rectSize/2
+//     let topleftY = centerY - rectSize/2
 
-    rect(topLeftX,topleftY,rectSize,rectSize);
+//     rect(topLeftX,topleftY,rectSize,rectSize);
 
 
-}
+// }
 
-function keyPressed(){
-    rectSize = 100;
-}
+// function keyPressed(){
+//     rectSize = 100;
+// }
 
-function keyReleased(){
-    rectSize = 50;
-}
+// function keyReleased(){
+//     rectSize = 50;
+// }
 
 //-------------------------------------------------Challenge 1
 
-function setup(){
-    createCanvas(400,400);
-    background(220);
-    noStroke();
-}
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     noStroke();
+// }
 
-function keyPressed(){
+// function keyPressed(){
 
-}
+// }
 
-function keyReleased(){
+// function keyReleased(){
     //counter??? - how many circle
-    
-}
+    /*for (let i=0; end; step);{
+
+        change size
+        change color
+        change position x
+        change position y
+} */
 floor(random(5,21))
 // let shapeColor = 'yellow';
 
@@ -86,3 +91,32 @@ floor(random(5,21))
 //     fill (shapeColor);
 //     circle(width/2, height/2, 100);
 // }
+
+
+
+
+// function draw(){
+//     background(220);
+
+//     if(key=='c'){
+//         circle(width/2, height/2, 200);
+//     }
+//     if (key=='t'){
+//         triangle(width/2, height/2 = 100, width/2 = 100, height/2 + 100, width/2 + 100, height/2 + 100);
+//     }
+//     if (key=='s'){
+//         //square(width/2, height/2 = 100, 200);
+//         rect(width/2 = 100, height/2 = 100, 200, 200);
+//     }
+
+// }
+
+
+
+function setup(){
+    createCanvas(600,600);
+}
+function keyPressed(){
+    console.log("key: ", key);
+    console.log("keyCode: ", keyCode);
+}
