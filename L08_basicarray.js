f34ba68de92f60c1f66180c6d74be13c1ecbbc79
tyn_Gwen <Gwenyining@gmail.com>
2025-09-21 09:05:04 +0000

    Sep 21, 2025, 9:05 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,40 +1,40 @@
 // write your codes here
 
 
-let x = 200-20;
-let y = 200-20; 
-let size = 40; 
-let speedX = 5; 
-let speedY = 5; 
-let shapeColor = 'blue'; 
+// let x = 200-20;
+// let y = 200-20; 
+// let size = 40; 
+// let speedX = 5; 
+// let speedY = 5; 
+// let shapeColor = 'blue'; 
 
-function setup() {
-    createCanvas(600, 400); 
-    // background(220); 
-}
+// function setup() {
+//     createCanvas(600, 400); 
+//     // background(220); 
+// }
 
-function draw() {
-    background(220); 
+// function draw() {
+//     background(220); 
 
-    if (x<= 0 || x >= width - size){ 
-        speedX *= -1; // speedX = speedX*-1
-        shapeColor = color(random(255), random(255),random(255)); 
-        //fill(random(255,random(255),random(255)); 
-    }
+//     if (x<= 0 || x >= width - size){ 
+//         speedX *= -1; // speedX = speedX*-1
+//         shapeColor = color(random(255), random(255),random(255)); 
+//         //fill(random(255,random(255),random(255)); 
+//     }
 
-     if (y <= 0 || y >= height - size) {  
-        speedY *= -1; // speedX = speedX*-1
-        shapeColor = color(random(255), random(255),random(255)); 
-        //fill(random(255,random(255),random(255)); 
-    }
-    x += speedX; //x = x + speedX
-    y += speedY;            
+//      if (y <= 0 || y >= height - size) {  
+//         speedY *= -1; // speedX = speedX*-1
+//         shapeColor = color(random(255), random(255),random(255)); 
+//         //fill(random(255,random(255),random(255)); 
+//     }
+//     x += speedX; //x = x + speedX
+//     y += speedY;            
    
-    noStroke(); 
-    fill(shapeColor); 
-    rect(x, y, size, size); 
+//     noStroke(); 
+//     fill(shapeColor); 
+//     rect(x, y, size, size); 
 
-}
+// }
 
 textSize(50,50)
 text("My name is Gwen")
