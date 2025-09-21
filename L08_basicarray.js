c7a7db60afb2daa9cfa14b501f301acbf20ea3e1
tyn_Gwen <Gwenyining@gmail.com>
2025-09-21 09:28:55 +0000

    Sep 21, 2025, 9:28 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -56,7 +56,7 @@ let favFoods = ["ice cream", "chicken rice", "nuggets", "char kway teow", "carro
 function setup(){
     createCanvas(400,400)
     textSize(24)
-    textAlign(CENTER, CENTER);
+    textAlign(LargestContentfulPaint, CENTER);
     textSize(24);
 }
 text("My favourite foods")
