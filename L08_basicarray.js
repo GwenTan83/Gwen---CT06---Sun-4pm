931e543ecacea07eee5e0c2d86c8b7d09db04926
tyn_Gwen <Gwenyining@gmail.com>
2025-09-21 08:44:16 +0000

    Sep 21, 2025, 8:44 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,19 +1,1 @@
 // write your codes here
-
-let x = 300;
-let y = 200; 
-let size = 30; 
-let speedX = 5; 
-let speedY = 5; 
-let shapeColor = 'blue'; 
-
-function setup() {
-    createCanvas(600, 400); 
-    background(220); 
-}
-
-function draw() {
-    //background(220); 
-     noStroke(); 
-    fill(shapeColor); 
-        circle(x, y, size); }
