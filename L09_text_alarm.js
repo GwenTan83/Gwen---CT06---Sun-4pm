114f035d382fa3d14230f7bf84eee41d4b4faafa
tyn_Gwen <Gwenyining@gmail.com>
2025-10-05 08:34:30 +0000

    Oct 5, 2025, 8:34 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -36,3 +36,35 @@
 
 ////////////////////////////////////////////////////////////
 //Task 3
+
+let countdown = 5;
+let interval;
+
+function setup() {
+    createCanvas(600,400)
+    textAlign(CENTER, CENTER);
+}
+
+function draw(){
+    background(220);
+
+    fill(0);
+    textSize(24);
+    text("Click to start timer", width/2, height-50);
+    fill('blue');
+    textSize(80);
+    text(countdown, width/2, height/2);
+
+}
+
+function mousePressed(){
+    interval = setInterval(updateCountdown, 1000);
+}
+
+function updateCountdown(){
+    if(countdown>0) {
+        countdown -=1;
+    } else {
+        clearInterval(interval);
+    }
+}
