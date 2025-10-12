9ef8e86c177a08d35648c4ce6e68ac0dcc126fce
tyn_Gwen <Gwenyining@gmail.com>
2025-10-12 08:25:53 +0000

    Oct 12, 2025, 8:25 AM

--- revision2.js
+++ revision2.js
@@ -3,11 +3,11 @@ let x = 300-1
 let speed = 2
 function setup(){
     createCanvas(600,400);
-    background(173, 216, 230)
+    background(173, 216, 230);
 }
 
 function draw(){
-    fill("red")
+    fill("red");
     circle(x,200,100);
 }
 
