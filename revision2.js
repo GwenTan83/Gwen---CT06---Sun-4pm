293dcbdcd2ea560187154c3cade665ca3ef09280
tyn_Gwen <Gwenyining@gmail.com>
2025-10-12 09:00:03 +0000

    Oct 12, 2025, 9:00 AM

--- revision2.js
+++ revision2.js
@@ -1,26 +1,38 @@
 // Task 1
-let x = 300;
-let speedX = 2;
-let dia = 40;
+// let x = 300;
+// let speedX = 2;
+// let dia = 40;
+// function setup(){
+//     createCanvas(600,400);
+// }
+
+// function draw(){
+//     background('lightblue');
+//     fill('red'); //fill(255, 0, 0)
+//     noStroke();
+
+//     if(x>=width-dia/2 || x<=dia/2){
+//         speedX *=-1;
+//     }
+
+//     x += speedX;
+//     circle(x, height/2, 40);
+// }
+
+//Task 2
+
+let arr = ["Pizza", "Pies", "Chocolates", "Chips",];
+
 function setup(){
-    createCanvas(600,400);
+    createCanvas(400,400);
 }
 
 function draw(){
-    background('lightblue');
-    fill('red'); //fill(255, 0, 0)
-    noStroke();
-
-    if(x>=width-dia/2 || x<=dia/2){
-        speedX *=-1;
-    }
-
-    x += speedX;
-    circle(x, height/2, 40);
+    background(0);
 }
 
-//Task 2
-let arr = ["Pizza", "Pies", "Chocolates", "Chips",];
+
+    
 
 
 
