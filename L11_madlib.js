b3c493f5c761a18fd6f6a78fadb30de034fe5664
tyn_Gwen <Gwenyining@gmail.com>
2025-10-19 09:23:43 +0000

    Oct 19, 2025, 9:23 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,6 +1,10 @@
 // write your codes here
 
 let textBox
+textBox1 = createInput()
+textBox1.position(width/2, height - 150);
+textBox1.input(display1);
+
 function setup(){
     createCanvas(200, 200);
 }
