d1ce050cdf9ac01bc39fe85c7e8674e450e010b6
tyn_Gwen <Gwenyining@gmail.com>
2025-10-19 09:27:50 +0000

    Oct 19, 2025, 9:27 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -7,11 +7,8 @@ textBox.input(display);
 
 function setup(){
     createCanvas(200, 200);
-    background(255);
 }
 
 function draw(){
-    text(colorPicker.value());
-    textAlign(LEFT);
-    textSize(16);
+
 }
