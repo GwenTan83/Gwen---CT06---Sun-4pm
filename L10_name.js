c0ebb7ee7c481943175170719326042f3411753a
tyn_Gwen <Gwenyining@gmail.com>
2025-10-19 09:09:50 +0000

    Oct 19, 2025, 9:09 AM

--- L10_name.js
+++ L10_name.js
@@ -86,7 +86,7 @@ function draw(){
     text("Choose your background color", 50, colorPicker.y+15);
 
     rect(50, 100, 500, 150, 500);
-
+    text
     text(text1, width/2, 150);
     text(text2, width/2, 200);
     
