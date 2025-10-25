ddc82a54c424bec1f6fa177895602e6f875372e4
tyn_Gwen <Gwenyining@gmail.com>
2025-10-25 17:08:00 +0000

    Oct 25, 2025, 5:08 PM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,24 +1,24 @@
 // write your codes here
 
-let shapeColor = 'blue'; // Initial color
+// let shapeColor = 'blue'; // Initial color
 
-function setup(){
-    createCanvas(400, 400);
-    background(220);
-}
+// function setup(){
+//     createCanvas(400, 400);
+//     background(220);
+// }
 
-function draw(){
-    fill(shapeColor);
-    ellipse(200, 200, 80, 80); // Draw a circle
-}
+// function draw(){
+//     fill(shapeColor);
+//     ellipse(200, 200, 80, 80); // Draw a circle
+// }
 
-function mousePressed(){
-    shapeColor = 'red'; // Change color on press
-}
+// function mousePressed(){
+//     shapeColor = 'red'; // Change color on press
+// }
 
-function mouseReleased(){
-    shapeColor = 'blue'; // Change color back on release
-}
+// function mouseReleased(){
+//     shapeColor = 'blue'; // Change color back on release
+// }
 
 function setup(){
     createCanvas(200, 200);
