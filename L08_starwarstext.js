fbe44d5f2d51ec26f98cf20579a776af31ac4bff
tyn_Gwen <Gwenyining@gmail.com>
2025-10-25 17:33:05 +0000

    Oct 25, 2025, 5:33 PM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -11,7 +11,7 @@ if (ballX - ballSize/2 <= 0 || ballX + ballSize/2 >= height){
     ballSpeedY = ballSpeedY * -1;
 }
 
-if (ballY - ballSize/2 <= 0 || ballY + ballSize)
+if (ballY - ballSize/2 <= 0 || ballY + ballSize/2 >)
 
 noStroke();
 fill(255);
