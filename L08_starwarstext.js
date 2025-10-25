d7d7354e0fdc29f69eb6889052de5412c7523aec
tyn_Gwen <Gwenyining@gmail.com>
2025-10-25 17:33:25 +0000

    Oct 25, 2025, 5:33 PM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -5,16 +5,16 @@ function draw(){
     
     ballX = ballX + ballSpeedX;
     ballY = ballY + ballSpeedY;
-}
 
 if (ballX - ballSize/2 <= 0 || ballX + ballSize/2 >= height){
     ballSpeedY = ballSpeedY * -1;
 }
 
 if (ballY - ballSize/2 <= 0 || ballY + ballSize/2 >= height){
-    
+
 }
 
 noStroke();
 fill(255);
 circle(ballX, ballY, ballSize);
+}
