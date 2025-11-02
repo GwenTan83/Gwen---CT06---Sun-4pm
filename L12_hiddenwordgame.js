46fadd656f772cc47a01f209d99820f53128ae08
tyn_Gwen <Gwenyining@gmail.com>
2025-11-02 09:28:01 +0000

    Nov 2, 2025, 9:28 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -4,4 +4,7 @@ let textBox;
 let arr = ["Guess the Hidden Word!", "Attempts: ", "Hint: "];
 function setup(){
     createCanvas(600, 600);
+    textBox = createInput(); 
+    textBox.position(width/2 - textBox.width/2, 100);
+    
 }
