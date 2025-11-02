d288664f014c1cf76c2172cd38af8403d9448cc9
tyn_Gwen <Gwenyining@gmail.com>
2025-11-02 09:05:21 +0000

    Nov 2, 2025, 9:05 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -155,5 +155,5 @@ function display(){
     
     let template = random(storyTemplates);
 
-    storyText = template.replace("{noun}", textBox1.value )
+    storyText = template.replace("{noun}", textBox1.value());
 }
