c658f6975fe00890c4ba3e8abf22d46f31ae243e
tyn_Gwen <Gwenyining@gmail.com>
2025-11-02 08:52:14 +0000

    Nov 2, 2025, 8:52 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -88,3 +88,59 @@
 
 /////////////////////////////////////////////////
 //Task 2
+let textBox1, textBox2, textBox3, textBox4, textBox5; 
+let arr = ["Enter a noun: ", "Enter a verb: ", "Enter a adjective: ", "Enter an adverb:", "Enter a place: "];
+let button;
+let displayText1, displayText2, displayText3, displayText4, displayText5; 
+let arr2 = [];
+
+function setup() {
+    createCanvas(600, 600); 
+
+    textBox1 = createInput(); 
+    textBox1.position(width/2 - textBox1.width/2, 100);
+    
+    textBox2 = createInput(); 
+    textBox2.position(width/2 - textBox2.width/2, 150);
+
+    textBox3 = createInput(); 
+    textBox3.position(width/2 - textBox3.width/2, 200);
+
+    textBox4 = createInput(); 
+    textBox4.position(width/2 - textBox4.width/2, 250);
+
+    textBox5 = createInput(); 
+    textBox5.position(width/2 - textBox5.width/2, 300);
+    
+    button = createButton('Generate Story');
+    button.position(width/2 - button.width/2, 350);
+    button.mousePressed(display);
+}
+
+function draw() {
+
+    background(220); 
+
+    for(let i=0; i<arr.length; i++) {
+        textSize(20);
+        text(arr[i], 50, 100 + 15 + i*50);
+    }
+    
+    for(let i=0; i < arr2.length; i++){
+        textSize(20);
+        text(arr2[i], 50, 400 + i*30);
+    }
+}
+
+function display(){
+    displayText1 = "noun: " + textBox1.value();
+    arr2.push(displayText1);
+    displayText2 = "verb: " + textBox2.value();
+    arr2.push(displayText2);
+    displayText3 = "adj: " + textBox3.value();
+    arr2.push(displayText3);
+    displayText4 = "adv: " + textBox4.value();
+    arr2.push(displayText4);
+    displayText5 = "place: " + textBox5.value();
+    arr2.push(displayText5);
+}
