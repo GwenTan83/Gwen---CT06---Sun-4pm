a3221b70162dd911cb65622dc48d8d706772533c
tyn_Gwen <Gwenyining@gmail.com>
2025-11-02 09:01:21 +0000

    Nov 2, 2025, 9:01 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -124,7 +124,7 @@ function setup() {
     "Did you hear about the {adj} {noun} that tried to {verb} {adv} near {place}?"
     ];
 
-    let template = random(storyT)
+    let template = random(storyTemplates);
 }
 
 function draw() {
