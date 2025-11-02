eba2c23467dcd9011923f0f386b6addce6cf578e
tyn_Gwen <Gwenyining@gmail.com>
2025-11-02 09:28:34 +0000

    Nov 2, 2025, 9:28 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -7,5 +7,9 @@ function setup(){
     createCanvas(600, 600);
     textBox = createInput(); 
     textBox.position(width/2 - textBox.width/2, 100);
+    button = createButton('Generate Story');
+    button.position(width/2 - button.width/2, 350);
+    button.mousePressed(display);
+
     
 }
